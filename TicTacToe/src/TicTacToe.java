bbbfb126ed242b3531061cf201a5e74a59be59eb
mebangit123 <mebannongrum333@gmail.com>
2021-04-19 11:40:35 +0530

    Creating a tic tac toe board

--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -3,5 +3,18 @@ public class TicTacToe {
 
 	public static void main(String[] args) {
 		System.out.println("Tic Tac Toe Game");
+		
+		char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+				{'-', '+', '-', '+', '-'},
+				{' ', '|', ' ', '|', ' '},
+				{'-', '+', '-', '+', '-'},
+				{' ', '|', ' ', '|', ' '}};
+		
+		for(char[] row : gameBoard) {
+			for(char c : row) {
+				System.out.print(c);
+			}
+			System.out.println();
+		}
 	}
 }
