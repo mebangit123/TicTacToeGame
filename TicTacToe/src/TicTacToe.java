9884986664ebaf93b1638605e25621063596c42e
mebangit123 <mebannongrum333@gmail.com>
2021-04-19 13:29:37 +0530

    Allow player to input letter X or O

--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class TicTacToe {
 
@@ -10,11 +12,69 @@ public class TicTacToe {
 				{'-', '+', '-', '+', '-'},
 				{' ', '|', ' ', '|', ' '}};
 		
+		printBoard(gameBoard);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your Placement: (1-9)");
+		int playerPos = sc.nextInt();
+		sc.close();
+		placePiece(gameBoard,playerPos,"player");
+		
+		Random rand = new Random();
+		int cpuPos = rand.nextInt(9) +1;
+		placePiece(gameBoard,cpuPos,"cpu");
+		printBoard(gameBoard);
+		
+	}
+	public static void printBoard(char[][] gameBoard) {
 		for(char[] row : gameBoard) {
 			for(char c : row) {
 				System.out.print(c);
 			}
 			System.out.println();
+		}
+	}
+		
+	public static void placePiece(char[][] gameBoard, int pos, String user) {
+		char symbol = ' ';
+		
+		if(user.equals("player")) {
+			symbol = 'X';
+		}
+		else if(user.equals("cpu")) {
+			symbol = 'O';
+		}
+		
+		switch(pos){
+			case 1:
+				gameBoard[0][0] = symbol;
+				break;
+			case 2:
+				gameBoard[0][2] = symbol;
+				break;
+			case 3:
+				gameBoard[0][4] = symbol;
+				break;
+			case 4:
+				gameBoard[2][0] = symbol;
+				break;
+			case 5:
+				gameBoard[2][2] = symbol;
+				break;
+			case 6:
+				gameBoard[2][4] = symbol;
+				break;
+			case 7:
+				gameBoard[4][0] = symbol;
+				break;
+			case 8:
+				gameBoard[4][2] = symbol;
+				break;
+			case 9:
+				gameBoard[4][4] = symbol;
+				break;
+				
+			default:
+				break;
 		}
 	}
 }
